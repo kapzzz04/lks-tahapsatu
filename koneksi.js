1b6554e61b9b0ce7e2fa7cd680872b9e518dc758
kapzzz04 <keyno596@gmail.com>
2023-11-25 20:53:30 +0700

    menambahkan file .env

--- koneksi.js
+++ koneksi.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
 // membuat koneksi database
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "S098765l@",
-  database: "dbregistrasi",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 conn.connect((err) => {

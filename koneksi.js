dc5fe2985ac7519f39bd0582b9f14f470785b493
kapzzz04 <keyno596@gmail.com>
2023-11-25 23:20:16 +0700

    menambahkan mysql server hosting

--- koneksi.js
+++ koneksi.js
@@ -6,6 +6,7 @@ const conn = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 conn.connect((err) => {
--- public/js/login.js
+++ public/js/login.js
@@ -19,10 +19,12 @@ const frm = document.querySelector("form");
 btn.addEventListener("click", async function (e) {
   const data = await fetchL();
   const seleksi = await menyeleksiUser(data.values);
+  console.log(seleksi);
   if (!seleksi) {
     alert("Invalid Username or Password");
     e.preventDefault();
   } else {
+    alert("berhasil login");
     frm.setAttribute("action", "/index");
     frm.submit(); // Kirim formulir secara programatik
     return false;
@@ -43,6 +45,7 @@ function menyeleksiUser(e) {
       element.username === username.value &&
       element.password === password.value
     ) {
+      console.log("hallo");
       return true; // Username sudah ada, hentikan pemrosesan
     }
   }
